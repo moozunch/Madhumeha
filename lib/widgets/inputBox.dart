0304f9f80ff370a8b977fcf9eb91641a31176c14
annisa :b <annisaputriaprilia12@gmail.com>
2025-03-09 18:23:11 +0700

    splash screen, primaryButton, dropDown, date and number picker

--- lib/widgets/inputBox.dart
+++ lib/widgets/inputBox.dart
@@ -1,46 +1,58 @@
 import 'package:flutter/material.dart';
 
+//Bisa untuk input string, sebagai datepicker, numberpicker.
 
 class InputBox extends StatelessWidget {
   final String label;
-  final TextEditingController controller; //perlu buat controller variabel tipe data TextEditingController
+  final TextEditingController controller;
   final String hint;
-  final TextInputType keyboardType; //menampung tipe data keyboard
+  final TextInputType keyboardType;
+  final VoidCallback? onTap; // untuk trigger DatePicker atau NumberPicker
 
-
-  //constructor
+  // Constructor
   const InputBox({
     super.key,
     required this.label,
-    required this.controller, //required karena controller wajib diisi
-    this.hint = '', //without required because it's optional
+    required this.controller,
+    this.hint = '',
     this.keyboardType = TextInputType.text,
+    this.onTap
   });
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start, //menentukan posisi textfield
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(label, style: Theme.of(context).textTheme.bodyMedium), //menampilkan label
+        Text(label, style: Theme.of(context).textTheme.bodyMedium),
         const SizedBox(height: 8,),
-        //input box textfield
-        TextField(
-          controller: controller,
-          keyboardType: keyboardType,
-          cursorColor: Theme.of(context).primaryColor,
-          style: TextStyle(color: Theme.of(context).secondaryHeaderColor),
-          decoration: InputDecoration(
-            hintText: hint,
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
-            contentPadding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Theme.of(context).primaryColor),
-              borderRadius: BorderRadius.circular(8),
-            )
+        GestureDetector(
+          onTap: onTap,
+          child: AbsorbPointer(
+            // agar input box tidak langsung aktifin keyboard
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(
+                maxWidth: 300,
+              ),
+              child: TextField(
+                controller: controller,
+                keyboardType: keyboardType,
+                cursorColor: Theme.of(context).primaryColor,
+                style: TextStyle(color: Theme.of(context).secondaryHeaderColor),
+                decoration: InputDecoration(
+                  hintText: hint,
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+                  contentPadding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Theme.of(context).primaryColor),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              ),
+            ),
           ),
         ),
-        const SizedBox(height: 16,), //jarak antar textfield ke2 nanti.
+        const SizedBox(height: 16,),
       ],
     );
   }
