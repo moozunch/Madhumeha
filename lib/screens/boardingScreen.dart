1c64458dd2b7606351161a01772a777cf0e29a52
annisa :b <annisaputriaprilia12@gmail.com>
2025-03-12 13:33:29 +0700

    boarding screen dan intent

---
+++ lib/screens/boardingScreen.dart
@@ -0,0 +1,38 @@
+import 'package:Madhumeha/screens/inputScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:Madhumeha/widgets/primaryButton.dart';
+
+
+class BoardingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.import_contacts),
+            SizedBox(height: 20,),
+            Text(
+              'Healthy lifestyle kamu dimulai sekarang. \n Before we start let’s fill all the information first',
+              style: Theme.of(context).textTheme.bodyMedium,
+              textAlign: TextAlign.center,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+            SizedBox(height: 10,),
+            PrimaryButton(
+                label: 'Start',
+                color: Theme.of(context).primaryColor,
+                textColor: Theme.of(context).scaffoldBackgroundColor,
+                width: 180,
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => InputScreen() ));}
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
